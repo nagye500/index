3643ffda759175c25c774766278e97dc8325a024
Erik Nagy Florian <81113352+nagye500@users.noreply.github.com>
2021-04-23 07:28:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
+const dropIcon = $('#drop');
+const head = $('#head');
+const logo = $('content logo a img');
+
 $(window).scroll(function() {
 	var $height = $(window).scrollTop();
-  if($height > 90) {
-		$('#head').addClass('active');
-	} else {
-		$('#head').removeClass('active');
+	head.removeClass('active');
+  if($height > 90 && !(dropIcon.hasClass('dropped') || head.hasClass('active'))) {
+		head.addClass('active');
+		dropIcon.addClass('active');
 	}
 });
+
+
 $(window).scroll(function() {
 	var $height = $(window).scrollTop();
+	$('#middle').removeClass('active');
   if($height > 950) {
 		$('#middle').addClass('active');
-	} else {
-		$('#middle').removeClass('active');
 	}
 });
 
 $(window).scroll(function() {
 	var $height = $(window).scrollTop();
+	$('#leftside').removeClass('active');
+
   if($height >= 1200) {
 		$('#leftside').addClass('active');
-	} else {
-		$('#leftside').removeClass('active');
 	}
 });
 
 $(window).scroll(function() {
 	var $height = $(window).scrollTop();
-  if($height > 120) {
-		$('#rightside').addClass('active');
-	} else {
-		$('#rightside').removeClass('active');
+	$('#anchor-to-top').removeClass('active');
+  if($height > 90) {
+		$('#anchor-to-top').addClass('active');
 	}
 });
- 
+
+
+$(window).scroll(function() {
+	var $height = $(window).scrollTop();
+	$('#rightside').removeClass('active');
+
+  if($height > 120) {
+		$('#rightside').addClass('active');
+  }
+});
+
+dropIcon.click(()=>{
+	dropIcon.toggleClass('dropped');
+	head.toggleClass('active');
+});
+if(window.innerWidth < 1400)
+{
+	logo.css('transform','scale',0.1);
+}
